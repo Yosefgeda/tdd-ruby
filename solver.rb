cbef3ae27972dc24505386f63ab3b7ac1f3aa94c
byiringiroscar <byiringoroscar@gmail.com>
2023-06-01 20:45:07 +0200

    Fix linters

--- solver.rb
+++ solver.rb
@@ -1,26 +1,24 @@
 class Solver
-    
-    def factorial(int)
-        if int == 1
-            return 1
-        else
-            return int * factorial(int - 1)
-        end
-    end   
+  def factorial(int)
+    return 1 if int == 1
 
-    def reverse(str)
-        str.reverse
+
+    int * factorial(int - 1)
+  end
+
+  def reverse(str)
+    str.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 5).zero?
+      'buzz'
+    elsif (num % 3).zero?
+      'fizz'
+    else
+      num.to_s
     end
-   
-    def fizzbuzz(n)
-        if n%3 == 0 && n%5 == 0
-            return 'fizzbuzz'
-        elsif n%5 == 0
-            return 'buzz'
-        elsif n%3 == 0
-            return 'fizz'
-        else
-            return n.to_s
-        end
-    end
+  end
 end
